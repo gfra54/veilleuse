8b3c0423b56a126674d65586d3d82c1ed4d4cea1
Gilles FRANCOIS <gilles@sopress.net>
2021-01-05 17:30:06 +0100

    sons

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,101 @@
 <template>
   <div id="app">
+    <div v-if="son" class="titre">{{son.titre}}</div>
+    <div class="balls">
+      <Ball v-for="i in 20"></Ball>
+    </div>
+
     <button @click="go"></button>
+
+    <audio v-if="son" ref="son" :src="son.url" preload loop></audio>
+
   </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
+  import Ball from './components/Ball.vue'
 
   export default {
     name: 'App',
+    components : {Ball},
+    data() {
+      return {
+        son:false,
+        rang:0,
+        sons : [
+        {
+          titre: 'Grenouilles',
+          url : 'https://sopress.s3.eu-central-1.amazonaws.com/veilleuse/frogs.mp3'
+        },
+        {
+          titre: 'Bruit blanc',
+          url : 'https://sopress.s3.eu-central-1.amazonaws.com/veilleuse/03-White-Noise-10min.mp3'
+        },
+        {
+          titre: 'Sèche linge',
+          url : 'https://sopress.s3.eu-central-1.amazonaws.com/veilleuse/16-Dryer-10min.mp3'
+        },
+        {
+          titre: 'Océan',
+          url : 'https://sopress.s3.eu-central-1.amazonaws.com/veilleuse/25-Ocean-10min.mp3'
+        },
+        {
+          titre: 'Pluie',
+          url : 'https://sopress.s3.eu-central-1.amazonaws.com/veilleuse/42-Rain-10min.mp3'
+        },
+        {
+          titre: 'Vagues',
+          url : 'https://sopress.s3.eu-central-1.amazonaws.com/veilleuse/45-Waves-10min.mp3'
+        }
+
+        
+        
+        ]
+      }
+    },
     methods: {
+      changerSon() {
+        if(this.$refs['son']) {
+          this.$refs['son'].pause();
+        }
+
+        this.son=this.sons[this.rang];
+        this.rang++;
+        if(this.rang==this.sons.length){
+          this.rang=0;
+        }
+        console.log(this.son.titre)
+
+        setTimeout(() => {
+          this.$refs['son'].play();
+        },100);
+
+      },
       go(){
         if (document.fullscreenEnabled) {
           if (!document.fullscreenElement) {
             document.documentElement.requestFullscreen();
-          } else {
-            //document.exitFullscreen();
           }
         }
+        this.changerSon();
+
 
       }
     },
     mounted(){
-      document.addEventListener("fullscreenchange", (event) => {
 
-      
-        if (!document.fullscreenElement) {
-        } else {
-        }
-      });
+      this.changerSon();
+
     }
   }
 </script>
 
 <style>
+.titre {
+  color:white;
+  font-family: "Comic sans ms";
+  margin:1em;
+}
 #app {
   position: fixed;
   top: 0;
@@ -43,7 +105,7 @@
   background: linear-gradient(124deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);
   background-size: 1800% 1800%;
 
-  animation: rainbow 60s ease infinite;
+  animation: rainbow 30s ease infinite;
 }
 
 button {
@@ -54,6 +116,16 @@ button {
   width: 100%;
   opacity: 0
 }
+
+.balls {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+
 @-webkit-keyframes rainbow {
   0%{background-position:0% 82%}
   50%{background-position:100% 19%}
---
+++ src/components/Ball.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="ball" ref="ball" :style="{background,left,top,transform,width,height}">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return  {
+        // colors : ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"],
+        colors : ["#FFFFFF"],
+        background : false,
+        left:0,
+        top:0,
+        transform:false,
+        width:0,
+        height:0,
+        to:{
+          x: 0,
+          y: 0
+        }
+
+      }
+    },
+    mounted() {
+      this.background = this.colors[Math.floor(Math.random() * this.colors.length)];
+
+      this.left = `${Math.floor(Math.random() * 100)}vw`;
+      this.top = `${Math.floor(Math.random() * 100)}vh`;
+      this.transform = `scale(${Math.random()})`;
+      this.width = `${Math.random()*10}em`;
+      this.height = this.width;
+      let random = Math.floor(Math.random() * 10);
+
+      this.to.x = Math.random() * ( random % 2 === 0 ? -11 : 11);
+      this.to.y = Math.random() * 12
+
+      let anim = this.$refs['ball'].animate(
+        [
+          { transform: "translate(0, 0)" },
+          { transform: `translate(${this.to.x}rem, ${this.to.y}rem)` }
+        ],
+          {
+            duration: (Math.random() + 1) * 2000,
+            direction: "alternate",
+            fill: "both",
+            iterations: Infinity,
+            easing: "ease-in-out"
+          }
+        );
+
+
+    }
+  }
+</script>
+
+<style>
+.ball {
+  position: absolute;
+  border-radius: 100%;
+  opacity: 0.9;
+}
+</style>
